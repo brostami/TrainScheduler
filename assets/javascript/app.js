691568cd785d807c853e66c929755bc87400387c
brostami <brostami@users.noreply.github.com>
2017-07-03 15:50:47 -0400

    Added firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyDT79EI7-_yH_hcFmy8IRaTsUnN5WArOQY",
+    authDomain: "trainscheduler-8b05c.firebaseapp.com",
+    databaseURL: "https://trainscheduler-8b05c.firebaseio.com",
+    projectId: "trainscheduler-8b05c",
+    storageBucket: "",
+    messagingSenderId: "837996515840"
+  };
+  firebase.initializeApp(config);
+
